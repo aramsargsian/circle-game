c1ee6aed0d96e2d30c29ee28f5f1fe60d7d75443
aramsargsian <sargsianaram@gmail.com>
2016-01-12 23:54:39 -0500

    Made a simple game. find the gold ball whatever. gonna come back and make it more complex

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	$('#gold').on('mouseenter', function(){
+				$(this).addClass('gold');
+				$(this).removeClass('circle');
+				$(this).animate({'border-radius': '100%', 'padding': '100px'}, 'fast');
+				$('.circle').addClass('post');
+
+				
+
+
+			});
+	$('.red').on('mouseenter', function(){
+		$(this).removeClass('red');
+		$(this).animate({'top': '-10px', 'border-radius': '100%'}, 'fast');
+		$(this).animate({'top': '0px'});
+		$(this).addClass('blue');
+		
+		
+	
+
+			$('.blue').on('mouseenter', function(){
+			$(this).removeClass('blue');
+			$(this).addClass('green');
+			});
+
+			$('.green').on('mouseenter', function(){
+			$(this).removeClass('green');
+			$(this).addClass('yellow');
+			});
+
+			$('.yellow').on('mouseenter', function(){
+			$(this).removeClass('yellow');
+			$(this).addClass('pink');
+			});
+
+			$('.pink').on('mouseenter', function(){
+			$(this).removeClass('pink');
+			$(this).addClass('orange');
+			});
+
+			$('.orange').on('mouseenter', function(){
+			$(this).removeClass('orange');
+			$(this).addClass('purple');
+			});
+
+			
+
+
+});
+	
+
+	
+});
